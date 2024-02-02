0e5f305416e355bb70dc2714465d50d80e83bc03
40Sabrina <sabrinaaaaa@hotmail.com>
2024-02-02 12:36:55 +0100

    add button Hutte Grotte and Piscine ok

--- rbnGrotte/src/App.jsx
+++ rbnGrotte/src/App.jsx
@@ -1,13 +1,11 @@
 import { Outlet} from 'react-router-dom'
 import Navbar from './Components/Navbar'
-import Filter from "./Components/Filter"
 
 function App() {
   
   return (
 <main>
   <Navbar/>
-  <Filter/>
 <Outlet/>
 </main>
   )
--- rbnGrotte/src/Components/Filter.jsx
+++ rbnGrotte/src/Components/Filter.jsx
@@ -2,55 +2,40 @@ import { useEffect, useState } from 'react';
 
 import "./Filter.css"
 
-import PictoTente from "../assets/PictoTente.png";
+import PictoHutte from "../assets/PictoTente.png";
 import PictoGrotte from "../assets/PictoGrotte.png";
 import PictoPiscine from "../assets/PictoPiscine.png";
 import PictoCascade from "../assets/PictoCascade.png";
 import PictoFeu from "../assets/PictoFeu.png";
 import PictoAnimaux from "../assets/PictoAnimaux.png";
 
-function Filter() {
-  const [Filter, setFiltre] = useState();
-
-  useEffect(() => {
-    fetch("http://localhost:4875/cave/all")
-      .then((res) => res.json())
-      .then((data) => {
-        setFiltre(data);
-        console.info(data);
-      })
-      .catch((error) => console.info(error));
-    }, []);
-
-  const handleFilterClick = (filterName) => {
-    setFiltre(filterName);
-  };
+function Filter({ handleFilterClick, handleFilterClickPiscine }){
 
   return (
     <>
     <section className="filtreLogo">
-      <button className='buttonImg'>
-      <img className="imgFiltre" src={PictoTente} alt="Pictogramme Tente" onClick={handleFilterClick} />
-        <h4 className='titleImg'>Tente</h4>
+      <button className='buttonImg'onClick={() => handleFilterClick("hutte")} >
+      <img className="imgFiltre" src={PictoHutte} alt="Pictogramme Hutte" />
+        <h4 className='titleImg'>Hutte</h4>
       </button>
         <button className='buttonImg'>
-        <img className="imgFiltre" src={PictoGrotte} alt="Pictogramme Grotte" onClick={handleFilterClick} />
+        <img className="imgFiltre" src={PictoGrotte} alt="Pictogramme Grotte" onClick={() => handleFilterClick("grotte")} />
         <h4 className='titleImg'>Grotte</h4>
         </button>
         <button className='buttonImg'>
-        <img className="imgFiltre" src={PictoPiscine} alt="Pictogramme Piscine" onClick={handleFilterClick} />
+        <img className="imgFiltre" src={PictoPiscine} alt="Pictogramme Piscine" onClick={() => handleFilterClickPiscine("piscine")} />
         <h4 className='titleImg'>Piscine</h4>
         </button>
         <button className='buttonImg'>
-        <img className="imgFiltre" src={PictoCascade} alt="Pictogramme Cascade" onClick={handleFilterClick} />
+        <img className="imgFiltre" src={PictoCascade} alt="Pictogramme Cascade" onClick={() => handleFilterClick("Cascade")} />
         <h4 className='titleImg'>Cascade</h4>
         </button>
         <button className='buttonImg'>
-        <img className="imgFiltre" src={PictoFeu} alt="Pictogramme Feu" onClick={handleFilterClick} />
+        <img className="imgFiltre" src={PictoFeu} alt="Pictogramme Feu" onClick={() => handleFilterClick("Feu")} />
         <h4 className='titleImg'>Feu</h4>
         </button>
         <button className='buttonImg'>
-        <img className="imgFiltre" src={PictoAnimaux} alt="Pictogramme Animaux" onClick={handleFilterClick} />
+        <img className="imgFiltre" src={PictoAnimaux} alt="Pictogramme Animaux" onClick={() => handleFilterClick("Animaux")} />
         <h4 className='titleImg'>Animaux</h4>
         </button>
     </section>
